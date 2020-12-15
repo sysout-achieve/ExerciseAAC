9d7540e068f2b5b4e747134320324071e9a3e2f9
geon <0408gun@gmail.com>
2020-12-16 00:46:54 +0900

    [feat] Room 적용(java)
    editText의 내용 Room을 이용해 저장하고 화면 출력 기능 추가

--- app/src/main/java/com/gunt/exerciseaac/data/AppDatabase.java
+++ app/src/main/java/com/gunt/exerciseaac/data/AppDatabase.java
@@ -2,9 +2,7 @@ package com.gunt.exerciseaac.data;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-
-import com.gunt.exerciseaac.data.domain.Todo;
-import com.gunt.exerciseaac.data.domain.TodoDAO;
+import com.gunt.exerciseaac.data.domain.*;
 
 @Database(entities = {Todo.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
--- app/src/main/java/com/gunt/exerciseaac/data/domain/Todo.java
+++ app/src/main/java/com/gunt/exerciseaac/data/domain/Todo.java
@@ -1,12 +1,10 @@
 package com.gunt.exerciseaac.data.domain;
 
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-import lombok.Data;
-
-@Data
 @Entity
 public class Todo {
     @PrimaryKey(autoGenerate = true)
@@ -15,5 +13,15 @@ public class Todo {
 
     public Todo(String title) {
         this.title = title;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer("TODO {");
+        sb.append("id=").append(id);
+        sb.append(", title= ").append(title).append("\n");
+        sb.append("}");
+        return super.toString();
     }
 }
