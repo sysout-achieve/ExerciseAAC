fb40e828693ac8c29cd645aec289a40d0ba5a191
geon <0408gun@gmail.com>
2020-12-18 22:16:59 +0900

    [feat] DataBinding 적용
    ui와 viewmodel의 데이터 binding을 통해 activity 코드 감소

--- app/src/main/java/com/gunt/exerciseaac/MainActivity.java
+++ app/src/main/java/com/gunt/exerciseaac/MainActivity.java
@@ -1,33 +1,23 @@
 package com.gunt.exerciseaac;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
-public class MainActivity extends AppCompatActivity {
+import com.gunt.exerciseaac.databinding.ActivityMainBinding;
 
-    TextView txt_list;
-    EditText et_todo;
-    AppDatabase db;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        txt_list = findViewById(R.id.txt_list);
-        et_todo = findViewById(R.id.et_todo);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
 
         MainViewModel viewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(MainViewModel.class);
-
-        viewModel.getAll().observe(this, todos -> txt_list.setText(todos.toString()));
-
-        findViewById(R.id.btn_input).setOnClickListener(v -> viewModel.insert(new Todo(et_todo.getText().toString())));
-
+        binding.setViewModel(viewModel);
     }
 
 }
--- app/src/main/java/com/gunt/exerciseaac/MainViewModel.java
+++ app/src/main/java/com/gunt/exerciseaac/MainViewModel.java
@@ -1,6 +1,7 @@
 package com.gunt.exerciseaac;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -14,20 +15,27 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainViewModel extends AndroidViewModel {
     AppDatabase db;
+    public LiveData<List<Todo>> todos;
+    public String todoStr;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
         db = Room.databaseBuilder(application, AppDatabase.class, "todo-db")
                 .build();
+        todos = getAll();
     }
 
-    public LiveData<List<Todo>> getAll(){
+    public LiveData<List<Todo>> getAll() {
         return db.todoDAO().getAll();
     }
 
-    public void insert(Todo todo){
-        Observable.just(todo)
+    public void insert(String todoStr) {
+        Observable.just(new Todo(todoStr))
                 .subscribeOn(Schedulers.io())
-                .subscribe(todoObj -> db.todoDAO().insert(todoObj));
+                .subscribe(todoObj -> {
+                    db.todoDAO().insert(todoObj);
+                    Log.d("Thread name: ", Thread.currentThread().getName());
+                });
+        Log.d("out Thread name: ", Thread.currentThread().getName());
     }
 }
